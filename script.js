9a7b8cfe4dfa2d97127574f340ceab7c92a6ca77
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2024-09-30 22:30:55 +0330

    five-day forecast

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
+const apiKey = "43b696b9d4c3a7541d4a9b0cbe41a3ac";
+
+const getWeatherDetails = (cityname, lat, lon) => {
+    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
+    });
+}
+
 const getCityDetails = () => {
     let cityName = cityInput.value.trim();
     if(!cityName) return;
